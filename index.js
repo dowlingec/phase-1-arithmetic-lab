6cef68bee6e27884ac2561da980a4d843373e617
Elizabeth Dowling <dowlinge94@gmail.com>
2022-03-22 10:58:15 -0400

    3 tests passing

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-// Write your code
+// Write your code here!
+let num1 = 31
+let num2 = 2
 
-// let multiply = num1 * num2
+const multiply = num1 * num2;
 
-const random = Math.floor ((Math.random() * 10) +1);
+const random = Math.floor(Math.random() * 10) 
 
-// const mod =
+let num3 = 9;
+let num4 = 5
 
+const mod = num3 % num4
